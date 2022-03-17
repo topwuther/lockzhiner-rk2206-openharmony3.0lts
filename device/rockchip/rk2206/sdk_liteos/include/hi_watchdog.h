b25266b9ff73404767984df23b7ac6a2e8fdcd57
lockzhiner_wang-xiaobin <wangxb@fzlzdz.com>
2022-03-17 18:29:07 +0800

    添加源代码的版权说明

--- device/rockchip/rk2206/sdk_liteos/include/hi_watchdog.h
+++ device/rockchip/rk2206/sdk_liteos/include/hi_watchdog.h
@@ -1,7 +1,5 @@
-/**
- * @file hi_watchdog.h
- *
- * Copyright (c) 2020 HiSilicon (Shanghai) Technologies CO., LIMITED.
+/*
+ * Copyright (c) 2022 FuZhou Lockzhiner Electronic Co., Ltd. All rights reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
